d8655bfbed79ff7b8c30d48a1396c74db4ceb4fc
robiwankenobi <rfnkurle@gmail.com>
2019-03-12 11:37:23 -0700

    Gif-Tastic Stage one

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+
+
+// var topics = ["Obi-Wan Kenobi", "Luke Skywalker", "Han Solo", "Darth Vader", "Grand Moff Tarkin", "Admiral Thrawn", "Emperor Palpatine", "Princess Leia Organa", "Chewbacca", 
+//             "C-3PO", "R2-D2", "Lando Calrissian", "Boba Fett", "Jabba the Hutt", "Darth Revan", "Darth Malak", "Padme Amidala"]
+
+// function renderButtons (){
+//     $("#buttons").empty()
+
+
+// for (var i = 0; i < topics.length; i++){
+//    var swButton = $=("<button>" + topics[i] + "</button>")
+// console.log(renderButtons)
+// } 
+   
+//    $("#buttons").append(swButton)
+// }
